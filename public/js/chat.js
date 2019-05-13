8066961e6cbb5389763327360346e7521e08a941
mun <quangnd.edu@gmail.com>
2019-05-13 17:11:56 +0700

    Add form state process

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,46 @@
 const socket = io();
 
+const messageForm = document.querySelector("#message-form");
+const messageInput = document.querySelector("#message");
+const btnSubmit = document.querySelector("#btnSubmit");
+const btnSendLocation = document.querySelector("#btnSendLocation");
+
 socket.on("message", mess => {
   console.log(mess);
 });
 
-document.querySelector("#message-form").addEventListener("submit", e => {
+messageForm.addEventListener("submit", e => {
   e.preventDefault();
-
+  btnSubmit.setAttribute("disabled", "disabled");
   const inputContent = e.target.elements.message.value;
 
   socket.emit("sendMessage", inputContent, error => {
     if (error) {
-      return console.log(error)
+      return console.log(error);
     }
-    console.log("Message delivered!");
+
+    setTimeout(() => {
+      btnSubmit.removeAttribute("disabled");
+      messageInput.value = "";
+      console.log("Message delivered!");
+    }, 2000);
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+btnSendLocation.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser.");
   }
 
+  btnSendLocation.removeAttribute("disabled");
   navigator.geolocation.getCurrentPosition(function(position) {
     const { latitude, longitude } = position.coords;
     socket.emit("sendLocation", { latitude, longitude }, error => {
+      btnSendLocation.removeAttribute("disabled");
       if (error) {
-        return alert(error)
+        return alert(error);
       }
-      console.log('Location shared')
+      console.log("Location shared");
     });
   });
 });
