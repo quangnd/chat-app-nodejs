b422c88b3909b3e7e30eb768bf4b7e773dc254f5
mun <quangnd.edu@gmail.com>
2019-05-13 16:03:56 +0700

    Share location using geo API

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,14 @@ document.querySelector("#message-form").addEventListener("submit", e => {
 
   socket.emit("sendMessage", inputContent);
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const {latitude, longitude} = position.coords
+    socket.emit('sendLocation', { latitude, longitude})
+  })
+})
--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 const express = require("express");
-const http = require('http')
+const http = require("http");
 const path = require("path");
 const chalk = require("chalk");
-const socketio = require('socket.io')
+const socketio = require("socket.io");
 
 const app = express();
-const server = http.createServer(app)
-const io = socketio(server)
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirPath));
 
-io.on('connection', (socket) => {
-  console.log('New Websocket connection')
+io.on("connection", socket => {
+  console.log("New Websocket connection");
 
   //Emit everybody except current client
-  socket.broadcast.emit('message', 'A new user has joined!')
-  socket.on('sendMessage', (message) => {
-    io.emit('message', message) //send message to all connected clients
-  })
+  socket.broadcast.emit("message", "A new user has joined!");
+  socket.on("sendMessage", message => {
+    io.emit("message", message); //send message to all connected clients
+  });
 
-  socket.on('disconnect', () => {
-    io.emit('message', 'A user has left!')
-  })
-})
-
-
-
-
+  socket.on("sendLocation", coords => {
+    const message = `https://google.com/maps?q=${coords.latitude},${coords.longitude}`;
+    io.emit("message", message);
+  });
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+});
 
 server.listen(PORT, () => {
   console.log(chalk.blue("Server is up on PORT " + PORT));
